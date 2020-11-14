e036cb2dbe3c2c770bc2dc7ad0fdc2da0745051d
itsgracian <gracian2020@gmail.com>
2020-11-14 20:11:24 +0200

    set page range

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -7,14 +7,54 @@ interface Props {
   currentPage: number;
   pageRangeDisplayed: number;
 }
+
 const PaginationComponent = (props: Props) => {
   const [currentNumber, setCurrentNumber] = useState<number>();
 
-  const { totalItems, setCurrentPage, currentPage, itemPerPage } = props;
+  const { totalItems, setCurrentPage, currentPage, itemPerPage, pageRangeDisplayed } = props;
 
   const [pages = [], setPages] = useState<number[]>();
 
+  const [pageRange = [], setPageRange] = useState<number[]>();
+
   const count = totalItems/itemPerPage;
+
+  const set = (item: number)=> {
+    const limit = itemPerPage * item;
+    const offset = limit - itemPerPage;
+    setCurrentPage({currentPage: item, limit, offset})
+  }
+
+  const nextPage = ()=>{
+    if(currentNumber){
+      if(count > currentNumber){
+      const next = currentNumber +1;
+      setCurrentNumber(next);
+      set(next);
+      const nextItem = next +1;
+
+      if(nextItem <= count){
+        setPageRange([next, nextItem]);
+      }else{
+        setPageRange([next])
+      }
+    }
+    }
+    return;
+  }
+
+  const previousPage = ()=>{
+    if(currentNumber && currentNumber > 0){
+      const prev = currentNumber - 1;
+      if(prev > 0){
+        setCurrentNumber(prev);
+        set(prev);
+        setPageRange([prev, prev +1]);
+      }
+      return;
+    }
+    return;
+  }
 
   useEffect(()=>{
     setCurrentNumber(currentPage);
@@ -26,46 +66,23 @@ const PaginationComponent = (props: Props) => {
       data.push(i+1);
     }
     setPages(data);
+    setPageRange(data.slice(0, pageRangeDisplayed));
     //eslint-disable-next-line
-  }, [])
+  }, []);
 
-  const nextPage = ()=>{
-    if(currentNumber){
-      if(count > currentNumber){
-      const next = currentNumber +1;
-      const limit = itemPerPage * next;
-      const offset = limit - itemPerPage;
-      setCurrentNumber(next);
-      setCurrentPage({currentPage: next, limit, offset });
-    }
-    }
-    return;
-  }
-
-  const previousPage = ()=>{
-    if(currentNumber && currentNumber > 0){
-      const prev = currentNumber - 1;
-      if(prev > 0){
-        const limit = itemPerPage * prev;
-        const offset = limit - itemPerPage;
-        setCurrentNumber(prev);
-        setCurrentPage({currentPage: prev, limit, offset });
-      }
-      return;
-    }
-    return;
-  }
 
   return (
     <>
       <div>
-        <ul className="pages">
+        {/* <ul className="pages">
         {pages && pages.length > 0 && pages.map((item, i)=><li key={i} 
-        className={item === currentNumber ? "activePage": ""} onClick={()=>{
-          const limit = itemPerPage * item;
-            const offset = limit - itemPerPage;
-            setCurrentPage({currentPage: item, limit, offset})
-        }}>{item}</li>)}
+        className={item === currentNumber ? "activePage": ""} onClick={()=>set(item)}>{item}</li>)}
+        </ul> */}
+
+        <ul className="pages">
+          {pageRange && pageRange.length > 0 && pageRange.map((item, i)=>
+          <li key={i} onClick={()=>set(item)} 
+          className={item === currentNumber ? "activePage": ""}>{item}</li>)}
         </ul>
         <div className="button">
         <button type="button" onClick={nextPage}>next</button>
